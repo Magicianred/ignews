beefd070241f0768dc65df099a35673e01f98fbb
igor-marchi <igoraugustomarchi@gmail.com>
2021-04-30 07:36:26 -0300

    crated pg posts preview

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
-import Link from 'next/link';
 import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
